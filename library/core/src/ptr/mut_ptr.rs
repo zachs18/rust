e801f3eaac59bec3a5b3c1cec7e6e96285554961
Zachary S <zasample18+github@gmail.com>
2025-10-09 21:30:46 -0500

    Change slice_as_array feature name to core_slice_as_array for `[_]` and `*(mut|const) [_]`.

--- library/core/src/ptr/mut_ptr.rs
+++ library/core/src/ptr/mut_ptr.rs
@@ -1708,8 +1708,8 @@ impl<T> *mut [T] {
     /// Gets a raw, mutable pointer to the underlying array.
     ///
     /// If `N` is not exactly equal to the length of `self`, then this method returns `None`.
-    #[stable(feature = "slice_as_array", since = "CURRENT_RUSTC_VERSION")]
-    #[rustc_const_stable(feature = "slice_as_array", since = "CURRENT_RUSTC_VERSION")]
+    #[stable(feature = "core_slice_as_array", since = "CURRENT_RUSTC_VERSION")]
+    #[rustc_const_stable(feature = "core_slice_as_array", since = "CURRENT_RUSTC_VERSION")]
     #[inline]
     #[must_use]
     pub const fn as_mut_array<const N: usize>(self) -> Option<*mut [T; N]> {
