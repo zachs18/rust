7688923a6987abe5169112bcafafd81996988987
Zachary S <zasample18+github@gmail.com>
2025-10-24 23:37:19 -0500

    Make Thin not dyn-compatible
    FIXME: make diagnostics not show "note: ... because it requires Self: Thin"
    if it also is showing "note: ... because it requires Self: Sized"

--- tests/ui/traits/ice-with-dyn-pointee-errors.rs
+++ tests/ui/traits/ice-with-dyn-pointee-errors.rs
@@ -7,13 +7,13 @@ use core::ptr::Thin;
 fn unknown_sized_object_ptr_in(_: &(impl Thin + ?Sized)) {}
 
 fn raw_pointer_in(x: &dyn Thin) {
-    //~^ ERROR the trait `Thin` is not dyn compatible
+    //~^ ERROR `Thin` is not dyn compatible
     unknown_sized_object_ptr_in(x)
     //~^ ERROR the trait bound `dyn Thin: Thin` is not satisfied
-    //~| ERROR the trait `Thin` is not dyn compatible
+    //~| ERROR `Thin` is not dyn compatible
 }
 
 fn main() {
     raw_pointer_in(&42)
-    //~^ ERROR the trait `Thin` is not dyn compatible
+    //~^ ERROR `Thin` is not dyn compatible
 }
