62925ed9dbd9dc5b58b6b109a2d4a488b9f07121
Zachary S <zasample18+github@gmail.com>
2026-03-02 20:19:36 -0600

    Relax core::ops::Drop to PointeeSized.
    Required to implement Drop for extern types and structs with such fields.

--- library/core/src/ops/drop.rs
+++ library/core/src/ops/drop.rs
@@ -204,7 +204,7 @@
 #[lang = "drop"]
 #[stable(feature = "rust1", since = "1.0.0")]
 #[rustc_const_unstable(feature = "const_destruct", issue = "133214")]
-pub const trait Drop {
+pub const trait Drop: crate::marker::PointeeSized {
     /// Executes the destructor for this type.
     ///
     /// This method is called implicitly when the value goes out of scope,
