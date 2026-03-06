e5c529644497d8b5ce47531088986146c519bc01
Zachary S <zasample18+github@gmail.com>
2026-03-06 08:59:06 -0600

    Implement type-checking for `offset_for_meta` for unions

--- compiler/rustc_hir_typeck/src/fn_ctxt/_impl.rs
+++ compiler/rustc_hir_typeck/src/fn_ctxt/_impl.rs
@@ -490,6 +490,18 @@ impl<'a, 'tcx> FnCtxt<'a, 'tcx> {
         }
     }
 
+    pub(crate) fn require_type_has_dynamic_alignment(
+        &self,
+        ty: Ty<'tcx>,
+        span: Span,
+        code: traits::ObligationCauseCode<'tcx>,
+    ) {
+        if !ty.references_error() {
+            let lang_item = self.tcx.require_lang_item(LangItem::MetaAligned, span);
+            self.require_type_meets(ty, span, code, lang_item);
+        }
+    }
+
     pub(crate) fn register_bound(
         &self,
         ty: Ty<'tcx>,
