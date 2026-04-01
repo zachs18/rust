d2b782a0d82b78dcae60c0dec66ae9c30af66739
Zachary S <zasample18+github@gmail.com>
2026-03-31 21:51:37 -0500

    [DO NOT MERGE] Revert "Create `Ty` type alias in `rustc_type_ir`" so rust-analyzer works again
    This reverts commit 17e0cc3ebd6562dde473e49207d1a79901b3fa2c.

--- compiler/rustc_type_ir/src/inherent.rs
+++ compiler/rustc_type_ir/src/inherent.rs
@@ -56,7 +56,7 @@ pub trait Ty<I: Interner<Ty = Self>>:
     fn new_alias(interner: I, alias_ty: ty::AliasTy<I>) -> Self;
 
     fn new_projection_from_args(interner: I, def_id: I::DefId, args: I::GenericArgs) -> Self {
-        Self::new_alias(
+        Ty::new_alias(
             interner,
             ty::AliasTy::new_from_args(interner, ty::AliasTyKind::Projection { def_id }, args),
         )
@@ -67,7 +67,7 @@ pub trait Ty<I: Interner<Ty = Self>>:
         def_id: I::DefId,
         args: impl IntoIterator<Item: Into<I::GenericArg>>,
     ) -> Self {
-        Self::new_alias(
+        Ty::new_alias(
             interner,
             ty::AliasTy::new(interner, ty::AliasTyKind::Projection { def_id }, args),
         )
