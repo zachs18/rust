784af53ce7b5d17251504c1e9db19fd2590e5518
Zachary S <zasample18+github@gmail.com>
2026-04-06 21:23:41 -0500

    Add ByRef and ByMut init adapters to explicitly reuse a borrowed initailizer

---
+++ library/core/src/init/adapters/by_mut.rs
@@ -0,0 +1,73 @@
+use crate::fmt;
+use crate::init::{InitMut, InitOnce, PinInitMut, PinInitOnce};
+use crate::marker::{MetaSized, PhantomData, PointeeSized};
+use crate::mem::MaybeUninit;
+use crate::ptr::Metadata;
+
+/// Initialize a place using initializer that can be reused.
+// This needs to be a separate type so it can mention `T`,
+// as otherwise `impl Init<T> for &U where U: Init<T>` overlaps with
+// `impl Init<T> for T` where `U: Init<&U>`, which a user could write.
+#[repr(transparent)]
+pub struct ByMut<T: MetaSized, I: PointeeSized> {
+    result: PhantomData<fn() -> T>,
+    /// The wrapped initializer.
+    pub init: I,
+}
+
+impl<T: MetaSized, I: PointeeSized + fmt::Debug> fmt::Debug for ByMut<T, I> {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("ByMut").field("init", &&self.init).finish_non_exhaustive()
+    }
+}
+
+impl<T: MetaSized, I: PointeeSized> ByMut<T, I> {
+    pub(crate) const fn new(init: &mut I) -> &mut Self {
+        // SAFETY: self is `#[repr(transparent)]` over `I` with no additional invariants.
+        unsafe { core::mem::transmute::<&mut I, &mut Self>(init) }
+    }
+}
+
+unsafe impl<T: MetaSized, Error, Arg, I: PointeeSized + PinInitMut<T, Error, Arg>>
+    PinInitOnce<T, Error, Arg> for &mut ByMut<T, I>
+{
+    fn metadata(this: &Self) -> Metadata<T> {
+        I::metadata(&this.init)
+    }
+
+    fn should_zero(this: &Self) -> bool {
+        I::should_zero(&this.init)
+    }
+
+    unsafe fn init_once(
+        this: Self,
+        dst: &mut MaybeUninit<T>,
+        arg: Arg,
+        pre_zeroed: bool,
+    ) -> Result<(), Error> {
+        // SAFETY: delegated to caller
+        unsafe { I::init_mut(&mut this.init, dst, arg, pre_zeroed) }
+    }
+}
+unsafe impl<T: MetaSized, Error, Arg, I: PointeeSized + InitMut<T, Error, Arg>>
+    InitOnce<T, Error, Arg> for &mut ByMut<T, I>
+{
+}
+unsafe impl<T: MetaSized, Error, Arg, I: PointeeSized + PinInitMut<T, Error, Arg>>
+    PinInitMut<T, Error, Arg> for &mut ByMut<T, I>
+{
+    unsafe fn init_mut(
+        this: &mut Self,
+        dst: &mut MaybeUninit<T>,
+        arg: Arg,
+        pre_zeroed: bool,
+    ) -> Result<(), Error> {
+        // SAFETY: delegated to caller
+        unsafe { I::init_mut(&mut this.init, dst, arg, pre_zeroed) }
+    }
+}
+unsafe impl<T: MetaSized, Error, Arg, I: PointeeSized + InitMut<T, Error, Arg>>
+    InitMut<T, Error, Arg> for &mut ByMut<T, I>
+{
+}
