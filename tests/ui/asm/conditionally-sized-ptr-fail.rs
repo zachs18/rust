1cb5a1d23401b2c75ce3e71befa1fc3e821a83f3
Zachary S <zasample18+github@gmail.com>
2026-04-08 14:20:40 -0500

    Allow passing pointers to any impl Thin to asm, not just extern type

--- tests/ui/asm/conditionally-sized-ptr-fail.rs
+++ tests/ui/asm/conditionally-sized-ptr-fail.rs
@@ -5,14 +5,14 @@ use std::arch::asm;
 fn _f<T: ?Sized>(p: *mut T) {
     unsafe {
         asm!("/* {} */", in(reg) p);
-        //~^ ERROR cannot use value of unsized pointer type `*mut T` for inline assembly
+        //~^ ERROR cannot use value of wide pointer type `*mut T` for inline assembly
     }
 }
 
 fn _g(p: *mut [u8]) {
     unsafe {
         asm!("/* {} */", in(reg) p);
-        //~^ ERROR cannot use value of unsized pointer type `*mut [u8]` for inline assembly
+        //~^ ERROR cannot use value of wide pointer type `*mut [u8]` for inline assembly
     }
 }
 
