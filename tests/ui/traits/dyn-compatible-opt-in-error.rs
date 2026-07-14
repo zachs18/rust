f326a86cab154b6b46a37153e8a131335721933d
Zachary S <zasample18+github@gmail.com>
2026-07-14 15:47:42 -0500

    Check that a trait is not both rustc_dyn_compatible_trait and rustc_dyn_incompatible_trait

--- tests/ui/traits/dyn-compatible-opt-in-error.rs
+++ tests/ui/traits/dyn-compatible-opt-in-error.rs
@@ -1,11 +1,10 @@
 //@ revisions: current next
 //@ ignore-compare-mode-next-solver (explicit revisions)
 //@[next] compile-flags: -Znext-solver
-//@ check-pass
+//@ check-fail
 // TODO: add errors
 
 #![feature(rustc_attrs)]
-
 
 #[rustc_dyn_compatible_trait]
 trait SizedSupertrait: Sized {}
@@ -19,5 +18,11 @@ trait DynIncompatible1 {
 trait DynIncompatible2 {
     fn dyn_incompatible(&self) where Self: std::fmt::Debug;
 }
+
+#[rustc_dyn_compatible_trait]
+//~^ NOTE trait was declared dyn-compatible here
+#[rustc_dyn_incompatible_trait]
+//~^ ERROR trait cannot be both
+trait OptInAndOut {}
 
 fn main() {}
